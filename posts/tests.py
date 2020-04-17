480dd237a9280f3971bb90714fbc7c178c158108
Johan Cañas <jxexcxo@gmail.com>
2020-04-16 20:03:49 -0500

    added tests

---
+++ posts/tests.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Post
+
+# Create your tests here.
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='just a text')
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'just a text')
+
+class HomePageViewTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='This is another text')
+
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
